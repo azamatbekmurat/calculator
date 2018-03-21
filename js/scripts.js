18d7aec8d0d8c5302dfb03bdea2ebd717e6b80e5
Guest User <Guest@epicodus-25.local>
2018-03-21 14:45:23 -0700

    add bootstrap forms, js business logic and front-end logic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,62 @@
+
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+    $("#output-add").text(result);
+  });
+
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+    $("#output-subtract").text(result);
+  });
+
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+    $("#output-multiply").text(result);
+  });
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+		var result =  divide(number1, number2);
+    $("#output-divide").text(result);
+  });
+
+
+
+});
+
+
 /*
 function getName(name) { return "hi " + name ; }
 getName("John");
@@ -65,7 +124,7 @@ function getInfo(name, age ,food ) {
 
 alert(getInfo(name,age,food) );
 */
-
+/*
 //Body Mass index calculator
 
 var calculateBMI = function(number1,number2) {
@@ -102,4 +161,4 @@ var gallon = parseInt(prompt("Enter gallon value: "));
 
 var liter = "Liter value is: " + parseInt(convertor(gallon));
 
-alert(showFahr);
+*/
